a9a1a631e668654592bfa861c1cbe5a3d69a1e1a
Ayush Tripathi <tripathi.at02@gmail.com>
2023-11-25 01:20:53 +0530

    :)

--- app/src/main/java/com/celes/botanysage/MainActivity.java
+++ app/src/main/java/com/celes/botanysage/MainActivity.java
@@ -21,6 +21,7 @@ import com.celes.botanysage.redditSubs.BotanyNormal;
 public class MainActivity extends AppCompatActivity {
     Button postBtn, memeBtn;
     Button floBtn, fruVegBtn;
+    //git check
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
